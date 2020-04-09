aa417b2134cac8ccc8b7a0969a0342054ba38a6e
HunnyJummani <hunny.jummani@botreetechnologies.com>
2020-04-09 15:05:02 +0530

    Fixed success + failure callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
-  post 'spree/payu_handler/success', to: 'spree/payu_handler#success'
-  post 'spree/payu_handler/fail'
+  post 'spree/payu_handler/success', to: 'payu_handler#success'
+  post 'spree/payu_handler/fail', to: 'payu_handler#fail'
 end
