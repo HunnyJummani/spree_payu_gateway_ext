0e6bfa7452c24e06c51e01766e9102bda5a09aaa
HunnyJummani <hunny.jummani@botreetechnologies.com>
2020-04-10 17:53:06 +0530

    Order Specs

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+require 'pry'
+
+describe Spree::Order, type: :model do
+
+  context "#confirmation_required?" do
+    let (:order) { Spree::Order.new }
+    it "returns false" do
+      allow_any_instance_of(Spree::Order).to receive(:confirmation_required?).and_return(false)
+
+      expect(order.confirmation_required?).to be_falsey
+      expect(order.confirmation_required?).not_to be_truthy
+    end
+  end
+end
